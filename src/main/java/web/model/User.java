de1f146b52b20c59f872b3f6e48592da13f78134
AlexanderVunder <alvund1994@gmail.com>
2022-08-15 11:42:11 +0700

    Init

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,9 +1,12 @@
 package web.model;
 
+import javax.persistence.*;
 import java.util.Objects;
-
+@Entity
+@Table(name = "users")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String lastName;
